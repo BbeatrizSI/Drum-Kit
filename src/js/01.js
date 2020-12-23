ce817b6d53f29a661063d189a8560c8888bfaac3
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-23 19:03:09 +0100

    keyCode reprecated

--- src/js/01.js
+++ src/js/01.js
@@ -1,8 +1,8 @@
 'use strict';
 
 window.addEventListener('keydown', function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
 
   if (!audio) return; // stop the function from running all together.
   audio.currentTime = 0; // rewind to the start
